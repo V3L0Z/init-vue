1e10291317ca191414fd8d7c3d0e07bc54dcfbb3
Gabriel Veloz <gabriel.veloz@live.com>
2020-11-28 20:42:29 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Calculator from "@/components/Calculator";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
+
 </script>
 
 <style>
@@ -23,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+require('../../revelry-hero/src/assets/styles/container.css')
+require('../../revelry-hero/src/assets/styles/hero.css')
+require('../../revelry-hero/src/assets/styles/mobile.css')
+require('../../revelry-hero/src/assets/styles/nav.css')
+require('../../revelry-hero/src/assets/styles/specialty.css')
